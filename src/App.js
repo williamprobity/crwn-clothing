c4471b10b130f234cd88e06c1c6f297e36e5685a
williamthein <william@probityweb.com.au>
2020-09-07 10:23:38 +1000

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -30,10 +30,8 @@ class App extends React.Component {
             currentUser: {
               id: snapShot.id,
               ...snapShot.data(),
-            },
+            }
           });
-
-          console.log(this.state);
         });
       }
 
