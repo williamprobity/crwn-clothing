dc8454a43d1f439de5a2a6dbce348ae50866b878
williamthein <william@probityweb.com.au>
2020-09-07 10:18:17 +1000

    changed all the files and folders with lesson 10

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-
 import { withRouter } from 'react-router-dom';
-// withRouter give access to history
-// ${mtch.url}${linkUrl} be aware of any space
+
 import './menu-item.styles.scss';
+
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-    <div className= {`${size} menu-item`}
-        onClick ={ () => history.push(`${match.url}${linkUrl}`) }
-    >
-        <div 
-        className='background-image' 
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
-        />
-        <div className='content'>
-            <div className='title'> {title.toUpperCase()} </div>
-            <div className='subtitle'> Shop Now</div>
-        </div>
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
+  </div>
 );
 
 export default withRouter(MenuItem);
